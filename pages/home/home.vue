ade55b639c8d93bbd475b04b65ddbcf920ebcf79
[username] <1805983094@qq.com>
2020-12-22 16:53:17 +0800

    初始化项目

---
+++ pages/home/home.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="home_container">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.home_container{
+	width: 100%;
+	height: 100%;
+	background-color: red;
+}
+</style>
