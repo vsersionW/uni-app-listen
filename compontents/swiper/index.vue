413b2ec33cadbfaf13423fe251c87f5de07e3798
[username] <1805983094@qq.com>
2020-12-22 21:19:54 +0800

    封装了swiper组件

---
+++ compontents/swiper/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <view class="swiper_container animated" hover-class="pulse">
+    <swiper
+      indicator-active-color="#ffffff"
+      class="swiper"
+      :indicator-dots="indicatorDots"
+      :autoplay="autoplay"
+      :interval="interval"
+      :duration="duration"
+    >
+      <swiper-item v-for="(item, index) in swiperImage" :key="index">
+        <image mode="widthFix" :src="item.image"></image>
+      </swiper-item>
+    </swiper>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      indicatorDots: true,
+      autoplay: true,
+      interval: 2000,
+      duration: 500,
+      swiperImage: [
+        { image: "../../static/swiper/01.png" },
+        { image: "../../static/swiper/02.png" },
+        { image: "../../static/swiper/03.png" },
+        { image: "../../static/swiper/04.png" },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.swiper_container {
+  width: 100%;
+  margin-top: 20rpx;
+  display: flex;
+  justify-content: center;
+}
+.swiper {
+  height: 400rpx;
+  width: 95%;
+}
+swiper-item {
+  width: 100%;
+  height: 100%;
+}
+swiper-item image {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -8,43 +8,21 @@
     </view>
     <!-- 搜索组件 -->
     <isSearch />
-    <!-- swiper -->
-    <view class="swiper_container animated" hover-class="pulse">
-      <swiper
-        indicator-active-color="#ffffff"
-        class="swiper"
-        :indicator-dots="indicatorDots"
-        :autoplay="autoplay"
-        :interval="interval"
-        :duration="duration"
-      >
-        <swiper-item v-for="(item,index) in swiperImage" :key="index">
-          <image mode='widthFix' :src='item.image'></image>
-        </swiper-item>
-      </swiper>
-    </view>
+    <!-- swiper组件 -->
+    <is-swiper/>
   </view>
 </template>
 
 <script>
+// swiper组件
+import isSwiper from "../../compontents/swiper";
 export default {
+  components:{
+    isSwiper
+  },
   data() {
     return {
-      background: ["color1", "color2", "color3"],
-      indicatorDots: true,
-      autoplay: true,
-      interval: 2000,
-      duration: 500,
-      swiperImage:[
-        {image:"../../static/swiper/01.png"},
-        {image:"../../static/swiper/02.png"},
-        {image:"../../static/swiper/03.png"},
-        {image:"../../static/swiper/04.png"},
-      ]
     };
-  },
-  onShow:function(){
-    console.log(this.swiperImage)
   },
   methods: {
     changeIndicatorDots(e) {
@@ -68,23 +46,5 @@ export default {
   width: 100%;
   height: 100%;
   box-sizing: border-box;
-}
-.swiper_container {
-  width: 100%;
-  margin-top: 20rpx;
-  display: flex;
-  justify-content: center;
-}
-.swiper{
-  height: 400rpx;
-  width: 95%;
-}
-swiper-item{
-  width: 100%;
-  height: 100%;
-}
-swiper-item image{
-  width: 100%;
-  height: 100%;
 }
 </style>
